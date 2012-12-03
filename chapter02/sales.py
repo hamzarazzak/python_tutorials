8feaa7b48a5a5267d643ae29e9031d903f4f5aa4
hamzarazzak <hrazzak01@gmail.com>
2012-12-03 16:26:53 -0500

    chapter 2 challenges complete

---
+++ chapter02/sales.py
@@ -0,0 +1,11 @@
+carprice = input('Enter the price of the car:')
+print(carprice)
+tax = carprice + 20 / 100
+license = carprice + 20 / 100
+dealerprep = 200
+destinationcharge = 200
+print('This is the total price: ', + tax + license + dealerprep + destinationcharge)
+print('The total price is detailed as follows: ') 
+print('Total costs for both tax and license costs are:', + tax + license)
+print('Total costs for both dealer prep and destination charges are:', + dealerprep + destinationcharge)
+
