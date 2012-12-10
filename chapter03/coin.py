4135bbd55cdb3fc8552e7c6bac54ecf17898ff5a
Hamza Razzak <hrazzak01@gmail.com>
2012-12-10 12:27:05 -0800

    Create coin.py

---
+++ chapter03/coin.py
@@ -0,0 +1,13 @@
+    import random
+    Num = int(raw_input("enter number of coin flips:"))
+    coinf = 0
+    h = 0
+    t = 0
+    while coinf < Num:
+    coinf = coinf + 1
+    rand = random.randint(0,2)
+    if rand == 1:
+    h = h + 1
+    else:
+    t = t + 1
+    print h, "heads",t, "tails"
