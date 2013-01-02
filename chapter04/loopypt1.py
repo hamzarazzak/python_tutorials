ee2b854497ec70c05dc569c92cc424b121b65960
hamzarazzak <hrazzak01@gmail.com>
2013-01-02 08:02:18 -0500

    create program to calculate resturant bill and tip

---
+++ chapter04/loopypt1.py
@@ -0,0 +1,9 @@
+dvd = ("moviea",
+       "movieb", 
+       "moviec")
+
+print("here is your collection")
+for d in dvd:
+    print (d)
+
+input("\nPress enter key to exit")
---
+++ python_beginners/restaurant.py
@@ -0,0 +1,7 @@
+
+total = int(input("Please enter the total amount: "))
+person = int(input("Please enter the number of people paying: "))
+per_person = total / person
+tip = total * 15 / 100
+print("Per person you have to pay :", per_person)
+print("Your tip for the total bill should be: ", tip)
